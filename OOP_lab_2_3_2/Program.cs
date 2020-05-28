5e1e3ae13343a0ad8bfbb4f345af9e83e793b709
Роман Данилевич <dwordchanel@gmail.com>
2020-05-28 23:29:23 +0300

    Add project files.

---
+++ OOP_lab_2_3_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace OOP_lab_2_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 1.25;
+
+            while (x <= 6.75)
+            {
+                double y = Math.Pow(x, 1 / 3) + Math.Log(3 * x);
+
+                Console.WriteLine("x = {0:f2}, y = {1:f4}", x, y);
+
+                x += 0.25;
+            }
+        }
+    }
+}
